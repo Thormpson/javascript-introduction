1cd79135f8a7f2480e32975ac19b1ecc6d026c72
THOMAS <akanothomas100@gmail.com>
2024-09-06 23:00:17 +0100

    datatype.js

---
+++ datatype.js
@@ -0,0 +1,57 @@
+
+// String 'gguf' "jhj" `hjkh`
+let name = "John Doe";
+console.log(name)
+// Number
+let age = 30;
+console.log(age)
+// Boolean
+let isRaining = true;
+console.log(isRaining)
+// Undefined unassigned variable
+let address;
+ console.log(address)
+
+// Null
+let car = null;
+console.log(car)
+
+// Object
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30
+};
+
+let color ={
+    white: "#ffffff",
+    black: "000000"
+}
+console.log(age)
+
+let basket ={
+    car: ["BMW", "Toyota", "Honda", "Benz", "Ford"],
+    color: ["red", "green", "blue"],
+    fruit: ["apple", "orange", "banana", "grape"]
+}
+    console.log(basket)
+// console.log(basket)
+// console.log("person:", person, "Colors:", color)
+
+// Array
+let colors = ["red", "green", "blue"];
+let cars = ["BMW", "Toyota", "Honda", "Benz", "Ford"];
+// console.log("cars:", cars)
+
+// Symbol
+let sym = Symbol('foo');
+console.log(sym)
+// console.log(sym)
+
+// BigInt
+let x = BigInt(123456789012345);
+let y = BigInt(7777777)
+console.log(x,y)
+
+let bigNumber = BigInt(1234567890123456789012345678901234567890);
+console.log(bigNumber)
